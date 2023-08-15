e2b791c24eb366bf0cb3ed40a1d0f51a656b9f0b
Engr-Pirlo <omarjibrilabdulkhadir@gmail.com>
2023-08-15 20:13:28 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="text-4xl text-pink-600 px-3 mt-3 text-center">eCommerce Project</h1>
+    </>
+  )
+}
+
+export default App
